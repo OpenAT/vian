0e47e78b44429321364c667da0d73a4bde84b46e
Michael Karrer <michaelkarrer81@gmail.com>
2019-11-13 16:12:38 +0100

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/vian_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'vian_config',
+    'summary': '''FS-Online vian instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance vian
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
